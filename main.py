47d0b80fccc8016633666291a4ea74f811c48ba5
Steven <steven.douillet@hiveo.fr>
2020-02-11 11:29:59 +0100

    add first viewPage with datas from db

--- main.py
+++ main.py
@@ -1,11 +1,70 @@
-from flask import Flask, render_template
+import sqlite3
+
+from flask import Flask, render_template, g
 
 app = Flask(__name__)
+
+DATABASE = 'database.db'
+
+"""
+#
+# DATABASE
+#
+"""
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+"""
+#
+# ROUTES
+#
+"""
 
 
 @app.route('/')
 def root():
-    return render_template('template.html', title="homePage")
+    cur = get_db().cursor()
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM items')
+        item_data = cur.fetchall()
+    conn.close()
+    items = parse(item_data)
+    return render_template('index.html', title="Accueil", items=items)
+
+
+"""
+#
+# PARSING DATABASE RESULTS
+#
+"""
+
+
+def parse(data):
+    ans = []
+    i = 0
+    while i < len(data):
+        curr = []
+        for j in range(7):
+            if i >= len(data):
+                break
+            curr.append(data[i])
+            i += 1
+        ans.append(curr)
+    return ans
 
 
 if __name__ == '__main__':
