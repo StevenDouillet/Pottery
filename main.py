d627f348dbf86d3279606a73af90f730f8c34f7e
quentiiinf <quentin.fornasier@gmail.com>
2020-03-10 11:07:18 +0100

    feat(login in system): add login in, registering and disconecting systems

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import hashlib
 import sqlite3
 
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, flash, url_for, session
+from werkzeug.utils import redirect
+
 
 app = Flask(__name__)
 
+app.secret_key = "stevensuce"
+
 DATABASE = 'database.db'
+
 
 """
 #
@@ -43,7 +49,78 @@ def root():
         item_data = cur.fetchall()
     conn.close()
     items = parse(item_data)
-    return render_template('index.html', title="Accueil", items=items)
+    return render_template('index.html', title="Accueil", items=items, session=session)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if session.get("email") is not None:
+        flash("Vous êtes déjà connecté")
+        return redirect(url_for('root'))
+
+    if "login" in request.form and "password" in request.form:
+        with sqlite3.connect('database.db') as conn:
+            cur = conn.cursor()
+            cur.execute(
+                'SELECT * FROM users WHERE email = ? AND password = ?',
+                (request.form["login"], hashlib.sha256(request.form["password"].encode()).hexdigest())
+            )
+            user = cur.fetchone()
+
+            if user is None:
+                flash("Mot de passe ou e-mail inconnus")
+                return redirect('login')
+
+            else:
+                session["email"] = user[3]
+                flash("Vous êtes connecté")
+                return redirect(url_for('root'))
+
+    else:
+        return render_template('login.html', title="Connexion", session=session)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash("Vous avez été déconnecté")
+    return redirect(url_for('root'))
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if session.get("email") is not None:
+        flash("Vous êtes déjà connecté")
+        return redirect(url_for('root'))
+
+    if "login" in request.form and "password" in request.form and "password2" in request.form:
+
+        if request.form["password"] != request.form["password2"]:
+            flash("Le mot de passe de confirmation n'est pas le même")
+            return redirect(url_for('register'))
+
+        with sqlite3.connect('database.db') as conn:
+            cur = conn.cursor()
+            cur.execute(
+                'SELECT * FROM users WHERE email = ?',
+                (request.form["login"], )
+            )
+            user = cur.fetchone()
+
+            if user is not None:
+                flash("Adresse email déjà utilisée")
+                return redirect(url_for('register'))
+
+            cur = conn.cursor()
+            cur.execute(
+                'INSERT INTO users (email, password) VALUES (?, ?)',
+                (request.form["login"], hashlib.sha256(request.form["password"].encode()).hexdigest())
+            )
+            flash("Bravo ! inscripton réussi ! vous pouvez désormais vous connectez")
+            return redirect(url_for('login'))
+
+    else:
+        return render_template('register.html', title="S'inscrire", session=session)
 
 
 """
